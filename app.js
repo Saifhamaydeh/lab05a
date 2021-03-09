cfa16328f5fdbef98cd45397ad73d413e09d46f7
saif hamaydeh <saif.ahmad12395@gmail.com>
2021-03-09 15:53:13 +0200

    sumAndMultiplyx

--- app.js
+++ app.js
@@ -51,12 +51,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-   
+   let d = a + b + c;
+   let e = a * b * c;
+   return [d , e];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
-
+sumAndMultiply(4,7,5); 
+console.log(sumAndMultiply(4,7,5));
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
